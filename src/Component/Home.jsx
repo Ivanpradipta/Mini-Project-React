56979fc8e527fe93517fc9602ad4e66508901abc
unknown <ivan.pradipta@ti.ukdw.ac.id>
2023-05-09 15:54:22 +0700

    Mini Project

--- src/Component/Home.jsx
+++ src/Component/Home.jsx
@@ -61,18 +61,25 @@ function Home() {
   };
 
   const handleUpdate = async (id) => {
+    const filesFolderRef = ref(storage, `products/${productImage.name}`);
+
     try {
-      console.log(id);
+      await uploadBytes(filesFolderRef, productImage);
+      const getLink = await getDownloadURL(
+        ref(storage, `products/${productImage.name}`)
+      );
       const product = {
         id: uuidv4(),
         name: productName,
         description: productDescription,
         status: productStatus,
+        image : getLink
       };
+      setGetLink(getLink);
       const productDoc = doc(db, "products", id);
       await updateDoc(productDoc, product);
       alert("berhasil update data");
-
+      console.log(product)
       getProducts();
     } catch (error) {
       console.error(error);
@@ -86,7 +93,6 @@ function Home() {
 
   const handleonClick = async (event) => {
     const filesFolderRef = ref(storage, `products/${productImage.name}`);
-
     try {
       await uploadBytes(filesFolderRef, productImage);
       const getLink = await getDownloadURL(
@@ -107,7 +113,7 @@ function Home() {
     } catch (error) {
       console.error(error);
     }
-
+    getProducts()
     setProductName("");
     setProductDescription("");
     setProductStatus("");
@@ -460,6 +466,20 @@ function Home() {
                               Reserved
                             </label>
                           </div>
+                          <label
+                            htmlFor="formFile"
+                            className="mb-2 inline-block text-neutral-700 dark:text-neutral-200"
+                          >
+                            Upload Gambar
+                          </label>
+                          <input
+                            className="relative m-0 block w-full min-w-0 flex-auto rounded border border-solid border-neutral-300 bg-clip-padding px-3 py-[0.32rem] text-base font-normal text-neutral-700 transition duration-300 ease-in-out file:-mx-3 file:-my-[0.32rem] file:overflow-hidden file:rounded-none file:border-0 file:border-solid file:border-inherit file:bg-neutral-100 file:px-3 file:py-[0.32rem] file:text-neutral-700 file:transition file:duration-150 file:ease-in-out file:[border-inline-end-width:1px] file:[margin-inline-end:0.75rem] hover:file:bg-neutral-200 focus:border-primary focus:text-neutral-700 focus:shadow-te-primary focus:outline-none dark:border-neutral-600 dark:text-neutral-200 dark:file:bg-neutral-700 dark:file:text-neutral-100 dark:focus:border-primary"
+                            type="file"
+                            id="formFile"
+                            onChange={(event) =>
+                              setProductImage(event.target.files[0])
+                            }
+                          />
                         </form>
                       </div>
                       <div className="flex items-center justify-end p-6 border-t border-solid border-blueGray-200 rounded-b">
